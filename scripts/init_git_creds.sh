af1d1a112587ff9d15d8c6554b918249fe1a290d
sportsanalystf <fk5ke@virginia.edu>
2026-05-25 19:19:02 +0000

    saving our two init files

---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER=fk5ke@virginia.edu
+NAME=sportsanalystf
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name ${NAME}
+
+git config --global --list
